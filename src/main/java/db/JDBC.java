c0e4119d4336956c5b4f54372463b49f3885f06d
Artur Teben <teben.artur99@gmail.com>
2020-08-20 23:58:51 +0300

    Delete unnecessary class

--- src/main/java/db/JDBC.java
+++
@@ -1,8 +0,0 @@
-package db;
-
-public class JDBC {
-    public static final String JDBC_DRIVER = "org.h2.Driver";
-    public static final String DB_URL = "jdbc:h2:mem:default;DB_CLOSE_DELAY=-1";
-    public static final String USER = "user";
-    public static final String PASS = "";
-}
